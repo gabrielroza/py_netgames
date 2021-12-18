54cfe83aa1261d7da665b9608600784e8a6d4432
Gabriel Machado <gabrielmachado616@hotmail.com>
2021-12-18 13:17:15 -0300

    Setup description

--- client/setup.py
+++ client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='client',
       version='1.0',
-      description='Model',
+      description='Client',
       author='Gabriel Machado',
       packages=find_namespace_packages(),
       install_requires=[
