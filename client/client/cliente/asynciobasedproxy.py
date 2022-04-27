899f47d9eae1cfc134bc5bc16084121c4002f66a
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-04-26 21:14:38 -0300

    AsyncIoBasedProxy listen

--- client/client/cliente/asynciobasedproxy.py
+++ client/client/cliente/asynciobasedproxy.py
@@ -6,34 +6,42 @@ from logging import Logger
 from uuid import UUID
 
 import websockets
+from model.messaging.deserializer import WebhookPayloadDeserializer
 from model.messaging.message import MatchRequestMessage, MoveMessage
+from model.messaging.webhook_payload import WebhookPayloadType
 from websockets import WebSocketServerProtocol
 
 
 class AsyncIoBasedProxy(ABC):
     _websocket: WebSocketServerProtocol
     _logger: Logger
+    _deserializer: WebhookPayloadDeserializer
 
     def __init__(self) -> None:
         super().__init__()
         self._logger = logging.getLogger("cliente.Proxy")
+        self._deserializer = WebhookPayloadDeserializer()
 
     @abc.abstractmethod
-    def _match_started(self):
+    async def _match_started(self, position: int):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def _receive_move(self, payload: any):
+    async def _receive_move(self, payload: any):
         raise NotImplementedError()
 
     async def connect(self, address: str):
         self._websocket = await websockets.connect("ws://" + address)
         self._logger.info("Connected to server")
-        asyncio.create_task(self._listen())
 
-    async def _listen(self):
+    async def listen(self):
         async for message in self._websocket:
-            self._logger.debug(f"Message received: {message}")
+            self._logger.info(f"Message received: {message}")
+            message = self._deserializer.deserialize(message)
+            if WebhookPayloadType.MATCH_STARTED == message.type():
+                await self._match_started(message.position)
+            elif WebhookPayloadType.MOVE == message.type():
+                await self._receive_move(message.payload)
 
     async def request_match(self, player_name: str, game_id: UUID, amount_of_players: int):
         payload = MatchRequestMessage(player_name, game_id, amount_of_players).to_payload().to_json()
@@ -52,4 +60,4 @@ class AsyncIoBasedProxy(ABC):
 
     async def _send_message(self, message: str):
         await self._websocket.send(message)
-        self._logger.debug(f"Message sent: {message}")
+        self._logger.info(f"Message sent: {message}")
