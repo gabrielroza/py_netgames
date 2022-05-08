7a7a76125c5ae137bd13a9ca6ffc0cd9e9f35111
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-05-07 21:37:13 -0300

    Sample methods

--- sample/ChallengeFactory.py
+++ sample/ChallengeFactory.py
@@ -7,11 +7,11 @@ class ChallengeFactory:
 
     def __init__(self) -> None:
         self._challenges = [
-            Challenge(left_jar=Jar(capacity=2, goal=-1), right_jar=Jar(capacity=3, goal=1), maximum_steps=2,
+            Challenge(left_jar=Jar(capacity=2, goal=None), right_jar=Jar(capacity=3, goal=1), maximum_steps=2,
                       description="Preencher 1 no jarro direito."),
-            Challenge(left_jar=Jar(capacity=6, goal=-1), right_jar=Jar(capacity=4, goal=2), maximum_steps=2,
+            Challenge(left_jar=Jar(capacity=6, goal=None), right_jar=Jar(capacity=4, goal=2), maximum_steps=2,
                       description="Preencher 2 no jarro direito."),
-            Challenge(left_jar=Jar(capacity=7, goal=-1), right_jar=Jar(capacity=5, goal=3), maximum_steps=2,
+            Challenge(left_jar=Jar(capacity=7, goal=None), right_jar=Jar(capacity=5, goal=3), maximum_steps=2,
                       description="Preencher 3 no jarro direito."),
         ]
         self._challenge_queue = self._challenges.copy()
--- sample/water_logic.py
+++ sample/water_logic.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from enum import Enum
+from typing import Optional
 
 from dataclasses_json import dataclass_json
 
@@ -14,17 +15,43 @@ class ChallengeState(Enum):
 @dataclass
 class Jar:
     capacity: int
-    goal: int
-    fill: int = field(default=2)
+    goal: Optional[int]
+    fill: int = field(default=0)
 
-    def goal_is_met(self):
-        return self.fill == self.goal
+    def goal_is_met(self) -> bool:
+        return self.goal is None or self.fill == self.goal
 
-    def is_full(self):
+    def is_full(self) -> bool:
         return self.fill == self.capacity
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return self.fill == 0
+
+    def empty(self) -> bool:
+        if self.is_empty():
+            return False
+        self.fill = 0
+        return True
+
+    def fulfill(self) -> bool:
+        if self.is_full():
+            return False
+        self.fill = self.capacity
+        return True
+
+    def transfer_to(self, destination: 'Jar') -> bool:
+        if self.is_empty() or destination.is_full():
+            return False
+
+        fulfill_amount = destination.capacity - destination.fill
+
+        if self.fill > fulfill_amount:
+            destination.fill = destination.capacity
+            self.fill = self.fill - fulfill_amount
+        else:
+            destination.fill = destination.fill + self.fill
+            self.fill = 0
+        return True
 
 
 @dataclass_json
