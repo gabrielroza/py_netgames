459efe269d934c2a82655040affc326ca717fd4e
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-05-29 11:54:11 -0300

    Pygame client methods

--- client/gameclient/pygameclient/PygameWebsocketProxy.py
+++ client/gameclient/pygameclient/PygameWebsocketProxy.py
@@ -2,22 +2,31 @@ import asyncio
 import threading
 from asyncio import AbstractEventLoop
 from threading import Thread
+from uuid import UUID
 
 import pygame
+from model.messaging.deserializer import WebhookPayloadDeserializer
+from model.messaging.message import MoveMessage, MatchRequestMessage
+from model.messaging.webhook_payload import WebhookPayloadType
 from websockets import client, WebSocketClientProtocol
 
 CONNECTED = pygame.event.custom_type()
+CONNECTION_ERROR = pygame.event.custom_type()
 RECEIVED = pygame.event.custom_type()
+MATCH_STARTED = pygame.event.custom_type()
+MOVE = pygame.event.custom_type()
 
 
 class PygameWebsocketProxy:
     _thread: Thread
     _loop: AbstractEventLoop
     _websocket: WebSocketClientProtocol
+    _deserializer: WebhookPayloadDeserializer
 
     def __init__(self) -> None:
         super().__init__()
         self._loop = asyncio.new_event_loop()
+        self.__deserializer = WebhookPayloadDeserializer()
 
         def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
             asyncio.set_event_loop(loop)
@@ -29,21 +38,40 @@ class PygameWebsocketProxy:
 
     def connect(self, address: str) -> None:
         async def async_connect():
-            self._websocket = await client.connect("ws://" + address)
-            return pygame.event.post(pygame.event.Event(CONNECTED, message={}))
+            try:
+                self._websocket = await client.connect("ws://" + address)
+                return pygame.event.post(pygame.event.Event(CONNECTED, message={}))
+            except Exception as e:
+                return pygame.event.post(pygame.event.Event(CONNECTION_ERROR, message={'exception': e}))
 
         self._run(target=async_connect)
 
-    def send(self, message: str) -> None:
+    def request_match(self, player_name: str, game_id: UUID, amount_of_players: int):
+        self._send(MatchRequestMessage(player_name, game_id, amount_of_players).to_payload().to_json())
+
+    def send_move(self, match_id: UUID, payload: any) -> None:
+        self._send(MoveMessage(match_id, payload).to_payload().to_json())
+
+    def _send(self, message: str) -> None:
         async def async_send():
-            await self._websocket.send(message)
+            try:
+                await self._websocket.send(message)
+            except Exception as e:
+                return pygame.event.post(pygame.event.Event(CONNECTION_ERROR, message={'exception': e}))
 
         self._run(target=async_send)
 
     def listen(self) -> None:
         async def async_listen():
-            async for message in self._websocket:
-                pygame.event.post(pygame.event.Event(RECEIVED, message=message))
+            try:
+                async for message in self._websocket:
+                    message = self.__deserializer.deserialize(message)
+                    if WebhookPayloadType.MATCH_STARTED == message.type():
+                        pygame.event.post(pygame.event.Event(MATCH_STARTED, message=message))
+                    elif WebhookPayloadType.MOVE == message.type():
+                        pygame.event.post(pygame.event.Event(MOVE, message=message))
+            except Exception as e:
+                return pygame.event.post(pygame.event.Event(CONNECTION_ERROR, message={'exception': e}))
 
         self._run(target=async_listen)
 
