b5348976faeb4b1315dc9520d644b0531208171e
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-06-04 17:39:08 -0300

    Reorder methods

--- sample/pygame_sample/TicTacToeInterface.py
+++ sample/pygame_sample/TicTacToeInterface.py
@@ -24,12 +24,12 @@ class TicTacToeInterface:
         self._is_turn = message.position == 0
         self._mark = TicTacToeMark.CROSS if self._is_turn else TicTacToeMark.CIRCLE
         self._surface = surface
+        self._websocket = websocket
         if self._is_turn:
             self._board = TicTacToeBoard()
         self._is_running = True
         self._setup()
         self._run()
-        self._websocket = websocket
 
     def _setup(self):
         self._surface.fill((255, 255, 255))
@@ -45,17 +45,16 @@ class TicTacToeInterface:
 
         pygame.display.update()
 
-    def _get_board_coordinate_from_click(self, mouse_position: Tuple[int, int]) -> Optional[TicTacToeCoordinate]:
-        x, y = mouse_position
-
-        def get_position(absolute_size, position) -> Optional[Literal[0, 1, 2]]:
-            first_third = absolute_size / 3
-            middle = absolute_size / 3 * 2
-            return 0 if position < first_third else 1 if position < middle else 2 if position < absolute_size else None
-
-        board_coordinate = get_position(WINDOW_HEIGHT, y), get_position(WINDOW_WIDTH, x)
-        print(board_coordinate)
-        return None if any(position is None for position in board_coordinate) else board_coordinate
+    def _run(self):
+        while self._is_running:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == MOUSEBUTTONDOWN:
+                    self._handle_click(pygame.mouse.get_pos())
+                elif event.type == MOVE_RECEIVED:
+                    self._handle_move_received(event.message)
+                elif event.type == CONNECTION_ERROR:
+                    self._is_running = False
 
     def _handle_click(self, mouse_position: Tuple[int, int]):
         if self._is_turn:
@@ -68,23 +67,21 @@ class TicTacToeInterface:
                 self._websocket.send_move(self.match_id, self._board)
                 self._is_turn = False
 
+    def _get_board_coordinate_from_click(self, mouse_position: Tuple[int, int]) -> Optional[TicTacToeCoordinate]:
+        x, y = mouse_position
+
+        def get_position(absolute_size, position) -> Optional[Literal[0, 1, 2]]:
+            first_third = absolute_size / 3
+            middle = absolute_size / 3 * 2
+            return 0 if position < first_third else 1 if position < middle else 2 if position < absolute_size else None
+
+        board_coordinate = get_position(WINDOW_HEIGHT, y), get_position(WINDOW_WIDTH, x)
+        print(board_coordinate)
+        return None if any(position is None for position in board_coordinate) else board_coordinate
+
     def _handle_move_received(self, message: MoveMessage):
         self._board = message.payload
         self._update_screen()
 
     def _update_screen(self):
         pass
-
-    def _defer_to_main_interface(self):
-        self._is_running = False
-
-    def _run(self):
-        while self._is_running:
-            events = pygame.event.get()
-            for event in events:
-                if event.type == MOUSEBUTTONDOWN:
-                    self._handle_click(pygame.mouse.get_pos())
-                elif event.type == MOVE_RECEIVED:
-                    self._handle_move_received(event.message)
-                elif event.type == CONNECTION_ERROR:
-                    self._defer_to_main_interface()
