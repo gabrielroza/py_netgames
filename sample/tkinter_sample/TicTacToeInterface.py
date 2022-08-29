3edd617e60eabd524a2de2cb771792e8c094c7ec
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-08-29 07:01:07 -0300

    Tkinter listen match start

--- sample/tkinter_sample/TicTacToeInterface.py
+++ sample/tkinter_sample/TicTacToeInterface.py
@@ -1,6 +1,7 @@
 from tkinter import Tk, Button
 from uuid import UUID
 
+from gameclient.tkinterclient.TkinterWebsocketListener import TkinterWebsocketListener
 from gameclient.tkinterclient.TkinterWebsocketProxy import TkinterWebsocketProxy
 from model.messaging.message import MatchStartedMessage, MoveMessage
 
@@ -10,14 +11,16 @@ from tkinter_sample import WINDOW_WIDTH, WINDOW_HEIGHT
 from tkinter_sample.ServerConnectionMenubar import ServerConnectionMenubar
 
 
-class TicTacToeInterface:
+class TicTacToeInterface(TkinterWebsocketListener):
     _tk: Tk
     _websocket: TkinterWebsocketProxy
     _game_id: UUID
-
-    _board: TicTacToeBoard
+    _menu_bar = ServerConnectionMenubar
+    _ongoing_match: bool
+    _match_id: UUID
     _is_turn: bool
     _mark: TicTacToeMark
+    _board: TicTacToeBoard
 
     def __init__(self) -> None:
         super().__init__()
@@ -31,11 +34,13 @@ class TicTacToeInterface:
         self._tk = _setup_tk()
         self._websocket = TkinterWebsocketProxy()
         self._game_id = UUID('b6625465-9478-4331-9e68-ffac2f02942f')
-        self.menu_bar = ServerConnectionMenubar(self._websocket, self._tk)
+        self._menu_bar = ServerConnectionMenubar(self._websocket, self._tk)
 
-        self._board = TicTacToeBoard()
+        self.ongoing_match = False
+        self.match_id = None
         self._is_turn = False
-        self._mark = TicTacToeMark.CROSS
+        self._mark = None
+        self._board = None
 
     def _update_screen(self):
         def build_button(coordinate: TicTacToeCoordinate, value: TicTacToeMark):
@@ -44,29 +49,35 @@ class TicTacToeInterface:
             if value:
                 button.configure(text=value)
 
-        [build_button(coordinate, value) for coordinate, value in self._board.get_coordinates().items()]
+        if self.ongoing_match:
+            [build_button(coordinate, value) for coordinate, value in self._board.get_coordinates().items()]
 
     def run(self):
-        self._tk.config(menu=self.menu_bar)
+        self._tk.config(menu=self._menu_bar)
         self._update_screen()
         self._websocket.add_listener(self)
         self._tk.mainloop()
 
     def _handle_click(self, coordinate: TicTacToeCoordinate):
         print(coordinate)
-        if self._is_turn and self._board.mark(self._mark, coordinate):
+        if self.ongoing_match and self._is_turn and self._board.mark(self._mark, coordinate):
             self._is_turn = False
             self._websocket.send_move(self.match_id, self._board.to_json())
             self._update_screen()
 
-    def match_started(self, match: MatchStartedMessage):
-        print(match)
+    def match_started(self, message: MatchStartedMessage):
+        self.ongoing_match = True
+        self.match_id = message.match_id
+        self._is_turn = message.position == 0
+        self._mark = TicTacToeMark.CROSS if self._is_turn else TicTacToeMark.CIRCLE
+        self._board = TicTacToeBoard() if self._is_turn else None
+        self._update_screen()
 
     def receive_move(self, message: MoveMessage):
         print(message)
-        # self._board = TicTacToeBoard.from_json(message.payload)
-        # self._is_turn = True
-        # self._update_screen()
+        self._board = TicTacToeBoard.from_json(message.payload)
+        self._is_turn = True
+        self._update_screen()
 
     def receive_disconnect(self):
         print("Disconnected")
