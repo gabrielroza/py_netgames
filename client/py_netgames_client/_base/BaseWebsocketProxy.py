df8a15962f37f5e6a006ead7cb3f7b55c0ba7c8a
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-09-14 19:35:11 -0300

    Issue warnings when proxy methods are called in a invalid state

--- client/py_netgames_client/_base/BaseWebsocketProxy.py
+++ client/py_netgames_client/_base/BaseWebsocketProxy.py
@@ -1,9 +1,10 @@
 import asyncio
 import threading
+import warnings
 from abc import ABC
 from asyncio import AbstractEventLoop
 from concurrent.futures import Future
-from typing import Dict
+from typing import Dict, Optional
 from uuid import UUID
 
 from py_netgames_model.messaging.deserializer import WebhookPayloadDeserializer
@@ -18,7 +19,7 @@ from py_netgames_client.id.IdentifierFileGenerator import IdentifierFileGenerato
 class BaseWebsocketProxy(ABC):
     _thread: threading.Thread
     _loop: AbstractEventLoop
-    _websocket: WebSocketClientProtocol
+    _websocket: Optional[WebSocketClientProtocol]
     _deserializer: WebhookPayloadDeserializer
     _game_id: UUID
 
@@ -27,6 +28,7 @@ class BaseWebsocketProxy(ABC):
         self._loop = asyncio.new_event_loop()
         self.__deserializer = WebhookPayloadDeserializer()
         self._game_id = game_id if game_id else IdentifierFileGenerator().get_or_create_identifier()
+        self._websocket = None
 
         def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
             asyncio.set_event_loop(loop)
@@ -35,7 +37,17 @@ class BaseWebsocketProxy(ABC):
         self._thread = threading.Thread(target=start_background_loop, args=(self._loop,))
         self._thread.start()
 
+    def _open(self):
+        return self._websocket and self._websocket.open
+
+    def _closed(self):
+        return not self._open()
+
     def send_connect(self, address: str, run_server_on_miss: bool = True) -> None:
+
+        if self._open():
+            return warnings.warn(f"Call to send_connect when connection is already active.", stacklevel=2)
+
         async def async_connect():
 
             async def attempt_connection():
@@ -57,6 +69,10 @@ class BaseWebsocketProxy(ABC):
         self._run(target=async_connect)
 
     def send_disconnect(self) -> None:
+
+        if self._closed():
+            return warnings.warn(f"Call to send_disconnect when there is no active connection", stacklevel=2)
+
         async def async_disconnect():
             try:
                 await self._websocket.close()
@@ -66,11 +82,19 @@ class BaseWebsocketProxy(ABC):
 
         self._run(target=async_disconnect)
 
-    def send_match(self, amount_of_players: int):
+    def send_match(self, amount_of_players: int) -> None:
+
+        if self._closed():
+            return warnings.warn(f"Call to send_match when there is no active connection", stacklevel=2)
+
         self._send(MatchRequestMessage(self._game_id, amount_of_players).to_payload().to_json(),
                    self._match_requested_success)
 
     def send_move(self, match_id: UUID, payload: Dict[str, any]) -> None:
+
+        if self._closed():
+            return warnings.warn(f"Call to send_move when there is no active connection", stacklevel=2)
+
         self._send(MoveMessage(match_id, payload).to_payload().to_json(), self._move_sent_success)
 
     def _receive_match_start(self, match: MatchStartedMessage):
