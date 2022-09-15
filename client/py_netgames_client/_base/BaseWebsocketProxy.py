b068da2c7f2da89266fd674189732bc8911369f1
Gabriel Machado <gabrielmachado616@hotmail.com>
2022-09-14 21:26:05 -0300

    Issue warnings in client

--- client/py_netgames_client/_base/BaseWebsocketProxy.py
+++ client/py_netgames_client/_base/BaseWebsocketProxy.py
@@ -1,10 +1,11 @@
 import asyncio
+import logging
 import threading
 import warnings
 from abc import ABC
 from asyncio import AbstractEventLoop
 from concurrent.futures import Future
-from typing import Dict, Optional
+from typing import Optional, Dict
 from uuid import UUID
 
 from py_netgames_model.messaging.deserializer import WebhookPayloadDeserializer
@@ -22,6 +23,7 @@ class BaseWebsocketProxy(ABC):
     _websocket: Optional[WebSocketClientProtocol]
     _deserializer: WebhookPayloadDeserializer
     _game_id: UUID
+    _logger: logging.Logger
 
     def __init__(self, game_id: UUID = None) -> None:
         super().__init__()
@@ -36,8 +38,17 @@ class BaseWebsocketProxy(ABC):
 
         self._thread = threading.Thread(target=start_background_loop, args=(self._loop,))
         self._thread.start()
+        self._logger = logging.getLogger("py_netgames_client")
 
     def send_connect(self, address: str, run_server_on_miss: bool = True) -> None:
+
+        if not isinstance(address, str):
+            return warnings.warn(self._invalid_type_message(address, "address", "send_connect", "str"), stacklevel=2)
+
+        if not isinstance(run_server_on_miss, bool):
+            return warnings.warn(
+                self._invalid_type_message(run_server_on_miss, "run_server_on_miss", "send_connect", "bool"),
+                stacklevel=2)
 
         if self._open():
             return warnings.warn(f"Call to send_connect when connection is already active.", stacklevel=2)
@@ -64,6 +75,11 @@ class BaseWebsocketProxy(ABC):
 
     def send_match(self, amount_of_players: int) -> None:
 
+        if not isinstance(amount_of_players, int):
+            return warnings.warn(
+                self._invalid_type_message(amount_of_players, "amount_of_players", "send_match", "int"),
+                stacklevel=2)
+
         if self._closed():
             return warnings.warn(f"Call to send_match when there is no active connection", stacklevel=2)
 
@@ -72,10 +88,31 @@ class BaseWebsocketProxy(ABC):
 
     def send_move(self, match_id: UUID, payload: Dict[str, any]) -> None:
 
+        if not isinstance(match_id, UUID):
+            return warnings.warn(
+                f"Invalid value {match_id} supplied as parameter match_id in call to send_move."
+                f" match_id should be the value received on match start (receive_match)",
+                stacklevel=2)
+
+        payload_type_description = "a dict with string keys and any permutation of the following basic types as values:" \
+                                   " dict, list, tuple, str, int, float, bool, None"
+
+        if not isinstance(payload, dict):
+            return warnings.warn(self._invalid_type_message(payload, "payload", "send_move", payload_type_description),
+                                 stacklevel=2)
+
         if self._closed():
             return warnings.warn(f"Call to send_move when there is no active connection", stacklevel=2)
 
-        self._send(MoveMessage(match_id, payload).to_payload().to_json(), self._move_sent_success)
+        try:
+            move_message = MoveMessage(match_id, payload).to_payload().to_json()
+        except TypeError as type_error:
+            self._logger.error(type_error)
+            return warnings.warn(
+                f"Error when serializing supplied payload {payload}." f" payload should be a {payload_type_description}",
+                stacklevel=2)
+
+        self._send(move_message, self._move_sent_success)
 
     def send_disconnect(self) -> None:
 
@@ -142,6 +179,10 @@ class BaseWebsocketProxy(ABC):
 
     def _closed(self):
         return not self._open()
+
+    def _invalid_type_message(self, value: any, parameter_name: str, method_name: str, actual_type: str) -> str:
+        return f"Invalid value {value} supplied as parameter {parameter_name} in call to {method_name}." \
+               f" {parameter_name} expects a {actual_type}"
 
     def _run(self, target) -> Future:
         return asyncio.run_coroutine_threadsafe(target(), self._loop)
